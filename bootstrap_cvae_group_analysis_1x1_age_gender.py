41afa88e4791284d9d3931b8fca163396d935c2c
soz223 <soz223@lehigh.edu>
2024-01-31 21:37:02 -0500

    finished ae traning

--- bootstrap_cvae_group_analysis_1x1_age_gender.py
+++ bootstrap_cvae_group_analysis_1x1_age_gender.py
@@ -10,57 +10,69 @@ References:
 """
 import argparse
 from pathlib import Path
-import os
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 from sklearn.metrics import roc_curve, auc
 from tqdm import tqdm
-
+import os
 from utils import COLUMNS_NAME, load_dataset, cliff_delta
+from scipy import interp
+from numpy import linspace
 
 PROJECT_ROOT = Path.cwd()
+result_focal_dir = './result_focal'
 
-result_baseline = './result_baseline/'
-
-
-def compute_brain_regions_deviations(diff_df, clinical_df, disease_label, mci_label, hc_label):
+def compute_brain_regions_deviations(diff_df, clinical_df, disease_label, hc_label=1):
     """ Calculate the Cliff's delta effect size between groups."""
     region_df = pd.DataFrame(columns=['regions', 'pvalue', 'effect_size'])
 
-    diff_hc = diff_df.loc[clinical_df['DIA'] == disease_label]
-    # add mci label in to the hc group
-    diff_hc = diff_hc.append(diff_df.loc[clinical_df['DIA'] == mci_label])
-
-
-    diff_patient = diff_df.loc[clinical_df['DIA'] == hc_label]
+    diff_hc = diff_df.loc[clinical_df['Diagnosis'] == disease_label]
+    diff_patient = diff_df.loc[clinical_df['Diagnosis'] == hc_label]
 
     for region in COLUMNS_NAME:
         _, pvalue = stats.mannwhitneyu(diff_hc[region], diff_patient[region])
         effect_size = cliff_delta(diff_hc[region].values, diff_patient[region].values)
- 
-        region_df = pd.concat([region_df, pd.DataFrame({'regions': region, 'pvalue': pvalue, 'effect_size': effect_size}, index=[0])], ignore_index=True)
+
+        region_df = region_df.append({'regions': region, 'pvalue': pvalue, 'effect_size': effect_size},
+                                     ignore_index=True)
+
     return region_df
 
-
-
-def compute_classification_performance(reconstruction_error_df, clinical_df, disease_label, mci_label, hc_label):
+def compute_classification_performance(reconstruction_error_df, clinical_df, disease_label, hc_label=1):
     """ Calculate the AUCs and accuracy of the normative model."""
-    error_hc = reconstruction_error_df.loc[clinical_df['DIA'] == hc_label]['Reconstruction error']
-    error_ad = reconstruction_error_df.loc[clinical_df['DIA'] == disease_label]['Reconstruction error']
-    error_mci = reconstruction_error_df.loc[clinical_df['DIA'] == mci_label]['Reconstruction error']
-    
-    error_patient = error_ad.append(error_mci)
+    error_hc = reconstruction_error_df.loc[clinical_df['Diagnosis'] == hc_label]['Reconstruction error']
+    error_patient = reconstruction_error_df.loc[clinical_df['Diagnosis'] == disease_label]['Reconstruction error']
 
     labels = list(np.zeros_like(error_hc)) + list(np.ones_like(error_patient))
-
-    print(np.zeros_like(error_hc))
-    
     predictions = list(error_hc) + list(error_patient)
 
     fpr, tpr, thresholds = roc_curve(labels, predictions)
     roc_auc = auc(fpr, tpr)
+    
+
+    # =============================================================================
+    # # only maximize sensitivity
+
+    # # Find the threshold where TPR is above the tpr_threshold
+    # # This will give us the threshold corresponding to the desired sensitivity
+    # tpr_threshold = 0.9
+    # sensitivity_indices = np.where(tpr >= tpr_threshold)
+    # optimal_idx = sensitivity_indices[0][0] if sensitivity_indices[0].size else 0
+    # optimal_threshold = thresholds[optimal_idx]
+
+    # # Compute accuracy using the optimal threshold
+    # predicted_labels = np.array(predictions > optimal_threshold, dtype=int)
+
+
+
+
+# =============================================================================
+
+    # Youden’s index optimization criterion (for both sensitivity and specificity)
 
     # Find the optimal threshold
     optimal_idx = np.argmax(tpr - fpr)
@@ -68,6 +80,10 @@ def compute_classification_performance(reconstruction_error_df, clinical_df, dis
 
     # Compute accuracy using the optimal threshold
     predicted_labels = [1 if e > optimal_threshold else 0 for e in predictions]
+
+
+
+
 
     accuracy = (np.array(predicted_labels) == np.array(labels)).mean()
     accuracy_in_hc = (np.array(predicted_labels)[np.array(labels) == 0] == 0).mean()
@@ -81,31 +97,30 @@ def compute_classification_performance(reconstruction_error_df, clinical_df, dis
     recall = TP / (TP + FN)
     specificity = TN / (TN + FP)
 
-    # print('Recall (Sensitivity):', recall)
-    # print('Specificity:', specificity)
 
-    return roc_auc, tpr, accuracy, accuracy_in_hc, accuracy_in_ad, recall, specificity
+    fixed_points = 100
+    base_fpr = linspace(0, 1, fixed_points)
+    interp_tpr = interp(base_fpr, fpr, tpr)
+    interp_tpr[0] = 0.0  # Ensuring that the curve starts at 0
 
+    print('shape of tpr', tpr.shape)
+    print('shape of predictions', np.array(predictions).shape)
+    return roc_auc, interp_tpr, accuracy, accuracy_in_hc, accuracy_in_ad, recall, specificity
 
-
-def main(dataset_name, comb_label):
+def main(dataset_name, comb_label, hz_para_list):
     """Perform the group analysis."""
     # ----------------------------------------------------------------------------
     n_bootstrap = 10
 
-    disease_label = 1
-    mci_label = 1
-    hc_label = 2
-    
+    dataset_name = 'ADNI'
+    disease_label = 0
 
-    model_name = 'supervised_ae'
+    model_name = 'supervised_cvae_age_gender'
 
-    # participants_path = PROJECT_ROOT / 'data' / dataset_name / 'participants.tsv'
-    # freesurfer_path = PROJECT_ROOT / 'data' / dataset_name / 'freesurferData.csv'
-    participants_path = PROJECT_ROOT / 'data' / 'y.csv'
-    freesurfer_path = PROJECT_ROOT / 'data' / 'av45.csv'
+    participants_path = PROJECT_ROOT / 'data' / dataset_name / 'participants.tsv'
+    freesurfer_path = PROJECT_ROOT / 'data' / dataset_name / 'freesurferData.csv'
 
-    
+    hc_label = 1
 
     # ----------------------------------------------------------------------------
     bootstrap_dir = PROJECT_ROOT / 'outputs' / 'bootstrap_analysis'
@@ -118,10 +133,12 @@ def main(dataset_name, comb_label):
 
     tpr_list = []
     auc_roc_list = []
-    accuracy_list = []
-    sensitivity_list = []
-    specificity_list = []
     effect_size_list = []
+    accuracy_list = []
+    accuracy_in_ad_list = []
+    accuracy_in_hc_list = []
+    recall_list = []
+    specificity_list = []
 
     for i_bootstrap in tqdm(range(n_bootstrap)):
         bootstrap_model_dir = model_dir / '{:03d}'.format(i_bootstrap)
@@ -141,25 +158,50 @@ def main(dataset_name, comb_label):
         # ----------------------------------------------------------------------------
         # Compute effect size of the brain regions for the bootstrap iteration
         diff_df = np.abs(normalized_df - reconstruction_df)
-        # region_df = compute_brain_regions_deviations(diff_df, clinical_df, disease_label, hc_label)
-        region_df = compute_brain_regions_deviations(diff_df, clinical_df, disease_label, mci_label, hc_label)
+        region_df = compute_brain_regions_deviations(diff_df, clinical_df, disease_label)
         effect_size_list.append(region_df['effect_size'].values)
         region_df.to_csv(analysis_dir / 'regions_analysis.csv', index=False)
 
         # ----------------------------------------------------------------------------
         # Compute AUC-ROC for the bootstrap iteration
-        # roc_auc, tpr,  = compute_classification_performance(reconstruction_error_df, clinical_df, disease_label)
-        # roc_auc, tpr, accuracy, accuracy_in_hc, accuracy_in_ad, recall, specificity = compute_classification_performance(reconstruction_error_df, clinical_df, disease_label, hc_label)
-        roc_auc, tpr, accuracy, accuracy_in_hc, accuracy_in_ad, recall, specificity = compute_classification_performance(reconstruction_error_df, clinical_df, disease_label, mci_label, hc_label)
+        roc_auc, tpr, accuracy, accuracy_in_hc, accuracy_in_ad, recall, specificity = compute_classification_performance(reconstruction_error_df, clinical_df, disease_label)
         auc_roc_list.append(roc_auc)
         tpr_list.append(tpr)
         accuracy_list.append(accuracy)
-        sensitivity_list.append(recall)
+        accuracy_in_hc_list.append(accuracy_in_hc)
+        accuracy_in_ad_list.append(accuracy_in_ad)
+        recall_list.append(recall)
         specificity_list.append(specificity)
+        
 
     (bootstrap_dir / dataset_name).mkdir(exist_ok=True)
     comparison_dir = bootstrap_dir / dataset_name / ('{:02d}_vs_{:02d}'.format(hc_label, disease_label))
     comparison_dir.mkdir(exist_ok=True)
+
+    # save auc roc and accuracy to txt file in result_focal folder
+    print('auc_roc_list\n', auc_roc_list)
+    # to save auc_roc_list to csv file
+
+    print('auc_roc: mean and std\n', np.mean(auc_roc_list), np.std(auc_roc_list))
+    print('accuracy_list\n', accuracy_list)
+    print('accuracy: mean and std\n', np.mean(accuracy_list), np.std(accuracy_list))
+    print('accuracy_in_hc_list\n', accuracy_in_hc_list)
+    print('accuracy_in_hc: mean and std\n', np.mean(accuracy_in_hc_list), np.std(accuracy_in_hc_list))
+    print('accuracy_in_ad_list\n', accuracy_in_ad_list)
+    print('accuracy_in_ad: mean and std\n', np.mean(accuracy_in_ad_list), np.std(accuracy_in_ad_list))
+    print('recall: mean and std\n', np.mean(recall_list), np.std(recall_list))
+    print('specificity: mean and std\n', np.mean(specificity_list), np.std(specificity_list))
+    print('\n\n\n')
+
+    # save auc roc and accuracy to txt file in result_focal folder
+    with open(os.path.join(result_focal_dir, 'result_focal_main_2.txt'), 'a') as f:
+        f.write('auc_roc: mean and std\n' + str(np.mean(auc_roc_list)) + ' ' + str(np.std(auc_roc_list)) + '\n')
+        f.write('accuracy: mean and std\n' + str(np.mean(accuracy_list)) + ' ' + str(np.std(accuracy_list)) + '\n')
+        # f.write('accuracy_in_hc: mean and std\n' + str(np.mean(accuracy_in_hc_list)) + ' ' + str(np.std(accuracy_in_hc_list)) + '\n')
+        # f.write('accuracy_in_ad: mean and std\n' + str(np.mean(accuracy_in_ad_list)) + ' ' + str(np.std(accuracy_in_ad_list)) + '\n')
+        f.write('sensitivity: mean and std\n' + str(np.mean(recall_list)) + ' ' + str(np.std(recall_list)) + '\n')
+        f.write('specificity: mean and std\n' + str(np.mean(specificity_list)) + ' ' + str(np.std(specificity_list)) + '\n')
+        f.write('\n\n\n')
 
     # ----------------------------------------------------------------------------
     # Save regions effect sizes
@@ -167,35 +209,45 @@ def main(dataset_name, comb_label):
     effect_size_df.to_csv(comparison_dir / 'effect_size.csv')
 
     # Save AUC bootstrap values
+# =============================================================================
+#     if os.path.exists(comparison_dir/'auc_rocs_mean_std_{:02d}.csv'.format(comb_label)):
+#         auc_rocs_mean_std_list = []
+#         auc_rocs_mean_std_list.append([comb_label,hz_para_list[0], hz_para_list[1], hz_para_list[2],np.mean(auc_roc_list),np.std(auc_roc_list)])
+#         auc_rocs_COL_NAME = ['Comb_Type', 'H_DIM1','H_DIM2','Z_DIM','Mean','Std']
+#         auc_rocs_mean_std_df = pd.DataFrame(columns=auc_rocs_COL_NAME, data=np.array(auc_rocs_mean_std_list))
+#         auc_rocs_mean_std_df.to_csv(comparison_dir/'cvae_auc_rocs_mean_std_{:02d}.csv'.format(comb_label),mode='a',header=False)   
+#             
+#     else:
+#         auc_rocs_mean_std_list = []
+#         auc_rocs_mean_std_list.append([comb_label,hz_para_list[0], hz_para_list[1], hz_para_list[2],np.mean(auc_roc_list),np.std(auc_roc_list)])
+#         auc_rocs_COL_NAME = ['Comb_Type', 'H_DIM1','H_DIM2','Z_DIM','Mean','Std']
+#         auc_rocs_mean_std_df = pd.DataFrame(columns=auc_rocs_COL_NAME, data=np.array(auc_rocs_mean_std_list))
+#         auc_rocs_mean_std_df.to_csv(comparison_dir/'cvae_auc_rocs_mean_std_{:02d}.csv'.format(comb_label))   
+# =============================================================================
+    
     auc_roc_list = np.array(auc_roc_list)
-    accuracy_list = np.array(accuracy_list)
-    sensitivity_list = np.array(sensitivity_list)
-    specificity_list = np.array(specificity_list)
-
-    with open(result_baseline + 'result_baseline.txt', 'a') as f:
-        f.write('Experiment settings: AE\n')
-        f.write('AUC-ROC: $ {:0.2f} \pm {:0.2f} $ \n'.format(np.mean(auc_roc_list) * 100, np.std(auc_roc_list) * 100))
-        f.write('Accuracy: $ {:0.2f} \pm {:0.2f} $ \n'.format(np.mean(accuracy_list) * 100, np.std(accuracy_list) * 100))
-        f.write('Sensitivity: $ {:0.2f} \pm {:0.2f} $ \n'.format(np.mean(sensitivity_list) * 100, np.std(sensitivity_list) * 100))
-        f.write('Specificity: $ {:0.2f} \pm {:0.2f} $ \n'.format(np.mean(specificity_list) * 100, np.std(specificity_list) * 100))
-        f.write('\n\n\n')
-
-
-    #np.savetxt("ae_auc_and_std.csv", np.concatenate((auc_roc_list, [np.std(auc_roc_list)])), delimiter=",")
+    np.savetxt("cvae_age_gender_auc_and_std.csv", np.concatenate((auc_roc_list, [np.std(auc_roc_list)])), delimiter=",")
     auc_roc_df = pd.DataFrame(columns=['AUC-ROC'], data=auc_roc_list)
     auc_roc_df.to_csv(comparison_dir / 'auc_rocs.csv', index=False)
 
     # ----------------------------------------------------------------------------
     # Create Figure 3 of the paper
+    
+    # randomly delete 3 values in tpr_list[0] and tpr_list[1]
+    
+
     tpr_list = np.array(tpr_list)
-    mean_tprs = tpr_list.mean(axis=0)
+    print('tpr_list\n', tpr_list)
+    mean_tprs = np.mean(tpr_list, axis=0)
+
+
     tprs_upper = np.percentile(tpr_list, 97.5, axis=0)
     tprs_lower = np.percentile(tpr_list, 2.5, axis=0)
     #plt.figure(figsize=(16, 20))
     plt.plot(np.linspace(0, 1, 100),
              mean_tprs,
              'b', lw=2,
-             label='ROC curve (AUC = {:0.3f} ; std = {:0.3f} ;95% CI [{:0.3f}, {:0.3f}])'.format(np.mean(auc_roc_list), np.std(auc_roc_list),
+             label='ROC curve (AUC = {:0.3f} ; 95% CI [{:0.3f}, {:0.3f}])'.format(np.mean(auc_roc_list),
                                                                                   np.percentile(auc_roc_list, 2.5),
                                                                                   np.percentile(auc_roc_list, 97.5)))
     plt.fill_between(np.linspace(0, 1, 100),
@@ -215,14 +267,14 @@ def main(dataset_name, comb_label):
 
     # --------------------------------------------------------------------------------------------
     # Create figure for supplementary materials
-    effect_size_df = effect_size_df.reindex(effect_size_df.mean().sort_values().index, axis=1)
+    effect_size_df = effect_size_df.reindex(abs(effect_size_df.mean()).sort_values().index, axis=1)
 
     plt.figure(figsize=(16, 20))
     plt.hlines(range(100),
-               np.percentile(effect_size_df, 2.5, axis=0),
-               np.percentile(effect_size_df, 97.5, axis=0))
+               abs(np.percentile(effect_size_df, 2.5, axis=0)),
+               abs(np.percentile(effect_size_df, 97.5, axis=0)))
 
-    plt.plot(effect_size_df.mean().values, range(100), 's', color='k')
+    plt.plot(abs(effect_size_df.mean().values), range(100), 's', color='k')
     plt.axvline(0, ls='--')
     plt.yticks(np.arange(100), effect_size_df.columns)
     plt.xlabel('Effect size')
@@ -235,7 +287,7 @@ def main(dataset_name, comb_label):
 
     # --------------------------------------------------------------------------------------------
     # Create Figure 4 of the paper
-    effect_size_sig_df = effect_size_df.reindex(effect_size_df.mean().sort_values().index, axis=1)
+    effect_size_sig_df = effect_size_df.reindex(abs(effect_size_df.mean()).sort_values().index, axis=1)
     lower_bound = np.percentile(effect_size_sig_df, 2.5, axis=0)
     higher_bound = np.percentile(effect_size_sig_df, 97.5, axis=0)
 
@@ -247,10 +299,10 @@ def main(dataset_name, comb_label):
 
     plt.figure()
     plt.hlines(range(n_regions),
-               np.percentile(effect_size_sig_df, 2.5, axis=0),
-               np.percentile(effect_size_sig_df, 97.5, axis=0))
+               abs(np.percentile(effect_size_sig_df, 2.5, axis=0)),
+               abs(np.percentile(effect_size_sig_df, 97.5, axis=0)))
 
-    plt.plot(effect_size_sig_df.mean().values, range(n_regions), 's', color='k')
+    plt.plot(abs(effect_size_sig_df.mean().values), range(n_regions), 's', color='k')
     plt.axvline(0, ls='--')
     plt.yticks(np.arange(n_regions), effect_size_sig_df.columns)
     plt.xlabel('Effect size')
@@ -258,15 +310,16 @@ def main(dataset_name, comb_label):
     plt.tight_layout()
     plt.savefig(comparison_dir / 'Significant_regions.pdf', format='pdf')
     plt.show()
-    
+    #plt.close()
+    #plt.clf()
     save = np.concatenate(([effect_size_sig_df.columns],
                            [abs(effect_size_sig_df.mean().values)],
                            [abs(np.percentile(effect_size_sig_df, 2.5, axis=0))],
                            [abs(np.percentile(effect_size_sig_df, 97.5, axis=0))]))
-    np.savetxt("ae_effect_size.csv", save, fmt='%s', delimiter=",")
-    #plt.close()
-    #plt.clf()
-
+    np.savetxt("cvae_effect_size.csv", save, fmt='%s', delimiter=",")
+    
+    # print success information
+    print('Group analysis successfully performed for {} vs {}.'.format(hc_label, disease_label))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -284,4 +337,4 @@ if __name__ == "__main__":
                         type=int)
     args = parser.parse_args()
 
-    main(args.dataset_name, args.comb_label)
+    main(args.dataset_name, args.comb_label, args.hz_para_list)
